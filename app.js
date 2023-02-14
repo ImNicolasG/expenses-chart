187748b734a5e0ee071f7ebed163927d39e965db
Nicolas Georgiou <nicog99@live.com>
2023-02-14 10:43:54 -0500

    Added json data into app.js because idk how to parse a local json file

--- app.js
+++ app.js
@@ -9,3 +9,42 @@ let todaysDay = newDay.getDay();
 document.getElementById(String(todaysDay)).style.backgroundColor = "hsl(186, 34%, 60%)";
 
 ///////////////////////////
+
+///// Data from JSON File /////
+
+const data = [
+    {
+      "day": "mon",
+      "amount": 17.45
+    },
+    {
+      "day": "tue",
+      "amount": 34.91
+    },
+    {
+      "day": "wed",
+      "amount": 52.36
+    },
+    {
+      "day": "thu",
+      "amount": 31.07
+    },
+    {
+      "day": "fri",
+      "amount": 23.39
+    },
+    {
+      "day": "sat",
+      "amount": 43.28
+    },
+    {
+      "day": "sun",
+      "amount": 25.48
+    }
+  ]
+
+
+console.log(data[4].day);
+
+
+/////////////////////////////////
